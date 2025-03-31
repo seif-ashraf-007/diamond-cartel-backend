78f6caa91addae51fcacf3253cbb98f9d3ff9e97
Seif Ashraf <177072202+seif-ashraf-007@users.noreply.github.com>
2025-03-31 23:34:48 +0200

    A mongoose schema for the user entity

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,4 +1,4 @@
-const mongoose = require('mongoose');
+import mongoose from "mongoose";
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
@@ -37,4 +37,4 @@ const userSchema = new Schema({
 
 const User = mongoose.model('User', userSchema);
 
-module.exports = User;
+export default User;
