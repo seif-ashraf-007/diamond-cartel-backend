2127fb2f1d49f3a8a546906d7f75537416ddd307
Seif Ashraf <177072202+seif-ashraf-007@users.noreply.github.com>
2025-03-31 21:31:04 +0200

    Basic Database connectivity

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 import express from "express";
 import { PORT, NODE_ENV } from "./config/env.js";
-
+import connectToDatabase from "./src/database/mongodb.js";
 const app = express();
 
 app.listen(PORT, async() => {
 	console.log(`[INFO] Server is running on port ${PORT} in ${NODE_ENV} mode`);
 	console.log(`[INFO] API: http://localhost:${PORT}/`);
+
+	await connectToDatabase();
 })
