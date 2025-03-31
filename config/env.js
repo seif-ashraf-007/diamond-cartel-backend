c469c728174d97dae0d999862abd4fa9b50e9746
Seif Ashraf <177072202+seif-ashraf-007@users.noreply.github.com>
2025-03-31 21:23:22 +0200

    Basic express app, js file to manage env variables

---
+++ config/env.js
@@ -0,0 +1,4 @@
+import { config } from "dotenv";
+config({ path: `./env/.env.${process.env.NODE_ENV || "development"}.local`});
+
+export const { NODE_ENV, PORT, DB_URI, JWT_SECRET, JWT_EXPIRES_IN } = process.env;
