270e1879459950e4fdb3deb5cdb6e64a6f0dd6a4
Seif Ashraf <177072202+seif-ashraf-007@users.noreply.github.com>
2025-03-31 23:33:42 +0200

    Using the auth routes - useing express.json and express.urlencoded - basic GET / for testing the backend connectivity

--- server.js
+++ server.js
@@ -1,7 +1,24 @@
 import express from "express";
 import { PORT, NODE_ENV } from "./config/env.js";
 import connectToDatabase from "./src/database/mongodb.js";
+import errorMiddleware from "./src/middlewares/error.middleware.js";
+import authRouter from "./src/routes/auth.routes.js";
+
 const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(errorMiddleware);
+
+app.use("/api/v1/auth", authRouter);
+
+app.get('/', (req, res) => {
+	res.status(200).json({
+		success: true,
+		message: "Welcome to the API",
+	})
+})
 
 app.listen(PORT, async() => {
 	console.log(`[INFO] Server is running on port ${PORT} in ${NODE_ENV} mode`);
